d0933428fdcf3ddbfc0ad556ff80103763df05b4
dxy (imp010) <daniel.yang17@gmail.com>
2023-01-30 16:38:13 -0500

    add wrapper to select key from obs_dict

--- rl/wrappers/__init__.py
+++ rl/wrappers/__init__.py
@@ -1,18 +1,36 @@
 import gym
+from gym import ObservationWrapper
 from gym.wrappers.flatten_observation import FlattenObservation
 
 from rl.wrappers.single_precision import SinglePrecision
 from rl.wrappers.universal_seed import UniversalSeed
 
 
-def wrap_gym(env: gym.Env, rescale_actions: bool = True) -> gym.Env:
+
+class DictToBoxWrapper(ObservationWrapper):
+    r"""Observation wrapper that selects one of the keys from a
+        dict obs space to expose to higher level consumers."""
+
+    def __init__(self, env: gym.Env, obs_key: str):
+        super(DictToBoxWrapper, self).__init__(env)
+        self.obs_key = obs_key
+        self.observation_space = env.observation_space[self.obs_key]
+
+    def observation(self, observation):
+        return observation[self.obs_key]
+
+
+def wrap_gym(env: gym.Env, rescale_actions: bool = True, obs_key: str = None) -> gym.Env:
     env = SinglePrecision(env)
     env = UniversalSeed(env)
     if rescale_actions:
         env = gym.wrappers.RescaleAction(env, -1, 1)
 
     if isinstance(env.observation_space, gym.spaces.Dict):
-        env = FlattenObservation(env)
+        if obs_key is None:
+            env = FlattenObservation(env)
+        else:
+            env = DictToBoxWrapper(env, obs_key)
 
     env = gym.wrappers.ClipAction(env)
 
